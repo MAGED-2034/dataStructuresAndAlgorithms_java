079c3d119eda14613bad30014c2583ff65fcb1ef
maged-2034 <test@noreplay>
2022-07-23 01:49:13 +0300

    week_1 assignment foldee created, and HelloGoodbye class added

---
+++ week_1/HelloGoodbye.java
@@ -0,0 +1,17 @@
+/*****
+ *  Algorithm part_1  princeton university
+ *  COURSERA
+ *  WEEK 1
+ * Assighenment_1's solution
+ * HelloGoodbye.java
+ *****/
+public class HelloGoodbye{
+    public static void main(String[] args){
+
+        String name1 = args[0];
+        String name2 =  args[1];
+        System.out.println("Hello "+name1 + " and "+name2 +".");
+        System.out.println("Goodbye " + name2 + " and " + name1 + ".");
+
+    }
+}
