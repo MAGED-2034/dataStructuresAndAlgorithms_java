57cbde16530ea2b29d6ef658675da2354aea06a1
maged-2034 <test@noreplay>
2022-07-24 03:09:03 +0300

    HelloWorld.java created and added

---
+++ week_1/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*****
+ *  Algorithm part_1  princeton university
+ *  COURSERA
+ *  WEEK 1
+ * Assighenment_1's solution
+ * HelloWorld.java
+ *****/
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
